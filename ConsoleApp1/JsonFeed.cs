6959452291d933c6a450425301894bee9c9c0db3
Igor Raspopovic <igor@raspopovic.ca>
2020-10-17 12:12:16 -0400

    working version

--- ConsoleApp1/JsonFeed.cs
+++ ConsoleApp1/JsonFeed.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
@@ -13,57 +14,80 @@ namespace ConsoleApp1
         static string _url = "";
 
         public JsonFeed() { }
-        public JsonFeed(string endpoint, int results)
+        //TODO - igor-result is used for what? - remove?
+		public JsonFeed(string endpoint)
         {
             _url = endpoint;
         }
-        
-		public static string[] GetRandomJokes(string firstname, string lastname, string category)
-		{
-			HttpClient client = new HttpClient();
-			client.BaseAddress = new Uri(_url);
-			string url = "jokes/random";
-			if (category != null)
-			{
-				if (url.Contains('?'))
-					url += "&";
-				else url += "?";
-				url += "category=";
-				url += category;
-			}
 
-            string joke = Task.FromResult(client.GetStringAsync(url).Result).Result;
 
-            if (firstname != null && lastname != null)
+		public static string[] GetRandomJokes(
+            string firstName,
+            string lastName,
+            string category,
+            int numOfJokes)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_url);
+            string url = "jokes/random";
+            if (category != null)
             {
-                int index = joke.IndexOf("Chuck Norris");
-                string firstPart = joke.Substring(0, index);
-                string secondPart = joke.Substring(0 + index + "Chuck Norris".Length, joke.Length - (index + "Chuck Norris".Length));
-                joke = firstPart + " " + firstname + " " + lastname + secondPart;
+                if (url.Contains('?'))
+                    url += "&";
+                else url += "?";
+                url += "category=";
+                url += category;
             }
+			string[] rc = new string[numOfJokes];
 
-            return new string[] { JsonConvert.DeserializeObject<dynamic>(joke).value };
+            for (int i = 0 ; i< numOfJokes; i++)
+			{
+				//TODO - make it more efficient async. C#8 has async streams
+				string jokeAsJsonStr = Task.FromResult(client.GetStringAsync(url).Result).Result;
+				string joke = JsonConvert.DeserializeObject<dynamic>(jokeAsJsonStr).value;
+				//it is better to replace string after we extract value
+				//TODO - check if one of them can be null but not the other?
+				if (firstName != null && lastName != null)
+            	{
+                	//TODO 1 - make this case insensitive comparison. Some jokes have upper case Chuck Norris
+					//TODO 2 - replace all occurrences of Chuck and Norris anywhere in the joke. Use replace?
+					var regex = new Regex("Chuck", RegexOptions.IgnoreCase); 
+					joke = regex.Replace(joke, firstName);
+					regex = new Regex("Norris", RegexOptions.IgnoreCase); 
+					joke = regex.Replace(joke, lastName);	
+
+					// int index = joke.IndexOf("Chuck Norris");
+                	// string firstPart = joke.Substring(0, index);
+                	// string secondPart = joke.Substring(0 + index + "Chuck Norris".Length, joke.Length - (index + "Chuck Norris".Length));
+                	// joke = firstPart + " " + firstname + " " + lastname + secondPart;
+            	}
+				rc[i] = joke;
+			}
+			return rc;
+        }
+        //igor - fixed : CamelCase function name, comment was wrong
+        //TODO- fix comment
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static dynamic GetNames()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_url);
+            var result = client.GetStringAsync("").Result;
+            return JsonConvert.DeserializeObject<dynamic>(result);
         }
 
-        /// <summary>
-        /// returns an object that contains name and surname
-        /// </summary>
-        /// <param name="client2"></param>
-        /// <returns></returns>
-		public static dynamic Getnames()
-		{
-			HttpClient client = new HttpClient();
-			client.BaseAddress = new Uri(_url);
-			var result = client.GetStringAsync("").Result;
-			return JsonConvert.DeserializeObject<dynamic>(result);
-		}
-
-		public static string[] GetCategories()
-		{
-			HttpClient client = new HttpClient();
-			client.BaseAddress = new Uri(_url);
-
-			return new string[] { Task.FromResult(client.GetStringAsync("categories").Result).Result };
-		}
+        public static string[] GetCategories()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_url);
+            //igor - end point to categories - was missing
+            string url = "jokes/categories";
+            var result = client.GetStringAsync(url).Result;
+            //igor - deserialize to proper type ->string[]
+            return JsonConvert.DeserializeObject<string[]>(result);
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Linq.Expressions;
+using System.Data.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -12,46 +14,65 @@ namespace ConsoleApp1
     {
         static string[] results = new string[50];
         static char key;
+        //igor - remove this. What is the point of this when mentod can return names
         static Tuple<string, string> names;
+
+        static List<string> categories = new List<string>();
         static ConsolePrinter printer = new ConsolePrinter();
 
         static void Main(string[] args)
         {
-            printer.Value("Press ? to get instructions.").ToString();
-            if (Console.ReadLine() == "?")
+            //what is the purpose of this prompt - TODO -igor -remove and add option below to exit the game with x
+            printer.Value("Welcome to Chuck Norris jokes game!").ToString();
+            //printer.Value("Press ? to get instructions.").ToString();
+            //if (Console.ReadLine() == "?")
             {
                 while (true)
                 {
                     printer.Value("Press c to get categories").ToString();
                     printer.Value("Press r to get random jokes").ToString();
+                    printer.Value("Press x to exit").ToString();
                     GetEnteredKey(Console.ReadKey());
                     if (key == 'c')
                     {
+                        //TODO - igor make this one function
                         getCategories();
                         PrintResults();
                     }
                     if (key == 'r')
                     {
+                        //igor - optimize the code
+                        string category = null;
+                        int numOfJokes = 1;
+
                         printer.Value("Want to use a random name? y/n").ToString();
                         GetEnteredKey(Console.ReadKey());
                         if (key == 'y')
                             GetNames();
+
                         printer.Value("Want to specify a category? y/n").ToString();
+                        //Igor - missed to get the category
+                        GetEnteredKey(Console.ReadKey());
                         if (key == 'y')
                         {
-                            printer.Value("How many jokes do you want? (1-9)").ToString();
-                            int n = Int32.Parse(Console.ReadLine());
-                            printer.Value("Enter a category;").ToString();
-                            GetRandomJokes(Console.ReadLine(), n);
+                            //IGOR - USER EXPERIENCE - THEY WILL NOT NOW CATEGORIES
+                            getCategories();
                             PrintResults();
+                            //TODO - IGOR - validate correct input and keep prompting until valid
+                            printer.Value("Enter a category:").ToString();                          
+                            category = Console.ReadLine();
                         }
-                        else
-                        {
-                            printer.Value("How many jokes do you want? (1-9)").ToString();
-                            int n = Int32.Parse(Console.ReadLine());
-                            GetRandomJokes(null, n);
-                            PrintResults();
-                        }
+                        //TODO - validate if user input is correct (number and in valid range) 
+                        printer.Value("How many jokes do you want? (1-9)").ToString();
+                        numOfJokes = Int32.Parse(Console.ReadLine());
+
+                        GetRandomJokes(category, numOfJokes);
+                        PrintResultsPerLine();
+                    }
+                    if (key == 'x')
+                    {
+                        printer.Value("Thanks for playing").ToString();
+                        Environment.Exit(0);
                     }
                     names = null;
                 }
@@ -63,9 +84,20 @@ namespace ConsoleApp1
         {
             printer.Value("[" + string.Join(",", results) + "]").ToString();
         }
+        private static void PrintResultsPerLine()
+        {
+            int count = 1;
+            foreach (string r in results)
+            {
+                printer.Value(count.ToString() + ". " + r).ToString();
+                count++;
+            }
+        }        
 
         private static void GetEnteredKey(ConsoleKeyInfo consoleKeyInfo)
         {
+            //reset key
+            key = ' ';
             switch (consoleKeyInfo.Key)
             {
                 case ConsoleKey.C:
@@ -104,25 +136,40 @@ namespace ConsoleApp1
                 case ConsoleKey.Y:
                     key = 'y';
                     break;
+                case ConsoleKey.X:
+                    key = 'x';
+                    break;    
             }
+            //Igor - easier to read
+            Console.WriteLine();
         }
 
         private static void GetRandomJokes(string category, int number)
         {
-            new JsonFeed("https://api.chucknorris.io", number);
-            results = JsonFeed.GetRandomJokes(names?.Item1, names?.Item2, category);
+            new JsonFeed("https://api.chucknorris.io");
+            results = JsonFeed.GetRandomJokes(names?.Item1, names?.Item2, category, number);
         }
 
         private static void getCategories()
         {
-            new JsonFeed("https://api.chucknorris.io", 0);
-            results = JsonFeed.GetCategories();
+            if (!categories.Any())
+            {
+                new JsonFeed("https://api.chucknorris.io");
+                results = JsonFeed.GetCategories();
+                categories.AddRange(results);
+            }
+            else
+            {
+                results = categories.ToArray();
+            }
+
         }
 
+        //igor - make it with param that indicates random vs input names
         private static void GetNames()
         {
-            new JsonFeed("https://www.names.privserv.com/api/", 0);
-            dynamic result = JsonFeed.Getnames();
+            new JsonFeed("https://www.names.privserv.com/api/");
+            dynamic result = JsonFeed.GetNames();
             names = Tuple.Create(result.name.ToString(), result.surname.ToString());
         }
     }
